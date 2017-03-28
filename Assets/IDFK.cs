73ab4bfb41f52a15abaaa0291e17814b32652107
mgagemorgan <gagemorgan@outlook.com>
2017-03-28 10:26:21 -0400

    Because Suter said to upload to the drive cloud google thang

---
+++ Assets/IDFK.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IDFK : MonoBehaviour {
+	public Transform prefab;
+	int x = 200;
+	// Use this for initialization
+	void Start () {
+		while (x > 0) {
+			Instantiate (prefab, new Vector3 (x, 10, 10), Quaternion.identity);
+			x -= 100;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
